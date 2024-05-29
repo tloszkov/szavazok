38932b27c695132d024742b27395f1dca0feddf6
Zsolt Kovacs <kzsolt82@gmail.com>
2024-05-30 00:30:33 +0300

    feat - Main components added

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent, CardHeader } from "./card"
+import CommunityLogo from "../../../public/Community.png"
+import FacebookLogo from "../../../public/facebook.svg"
+import InstagramLogo from "../../../public/instagram.svg"
+
+function Footer() {
+    return (
+       <div className="footer"> 
+        <p> </p>
+          <Card>
+            <CardContent>
+          <img src={CommunityLogo} width={60} height={60} alt="Logo"></img>
+          </CardContent>
+          </Card>
+          <Card>
+                <CardHeader>Egy Esély a Közösségért Egyesület</CardHeader>
+                    <CardContent>Székelykeresztúr</CardContent>
+                    <CardContent>Hargita megye - 535400</CardContent>
+                    <CardContent>Email - office@ospc.ro</CardContent>
+          </Card>
+
+          <Card>
+            <CardContent>
+            <a href="https://www.facebook.com/ospcom">
+                <img src={FacebookLogo} width={80} height={80} alt="Facebook Logo"/> 
+            </a>
+            <a href="https://www.instagram.com/ospc.egyesulet/">
+                <img src={InstagramLogo} width={80} height={80} alt="Instagram Logo"/> 
+            </a>
+            </CardContent>
+          </Card>
+       </div>
+    )
+}
+
+export default Footer
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,27 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
 
 function Home() {
 
-    return (
-      <>
-        <h1>Home</h1>
-      </>
-    )
-  }
+  return (
+    <>
+    <h1>Home</h1>
+    <Card>
+      <CardHeader className="cardHeader">Székelykeresztúr Betfalva</CardHeader>
+      <CardContent>
+            Sorszám 75.
+      </CardContent>
+      <CardContent>
+          Betlfavi kultúrház
+      </CardContent>
+      <CardContent>
+      Betfalva, Erzsébetkút és Bözsefalva utca, Nyikó sétány
+      </CardContent>
+      
+    </Card>
+    </>
+  )
+}
   
   export default Home
   
