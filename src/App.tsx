3104903bcc67359f591705f24be15125fe10d32f
Zsolt Kovacs <kzsolt82@gmail.com>
2024-05-29 21:24:59 +0300

    feat - Shadcn components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
-import Home from './pages/home/home';
 import About from './pages/about/about';
 import Voting from './pages/voting/voting';
+import Header from './components/ui/header';
+import Home from './pages/home/home';
 
 function App() {
 
   return (
     <>
+    <Header>
+    </Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
