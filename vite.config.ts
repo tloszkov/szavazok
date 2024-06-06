cea0825aeb9c011eeac131593b349f5d3003cb6e
Zsolt Kovacs <kzsolt82@gmail.com>
2024-06-06 22:29:27 +0300

    fix - vite.config.ts file modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
 
 // https://vitejs.dev/config/
 const _plugins = [react()];
-_plugins.unshift(MillionLint.vite())
 export default defineConfig({
   plugins: _plugins,
   resolve: {
